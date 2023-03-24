70d5a56a894910589169a9c04541fb198a477c32
ZhvankoAnna <114514755+ZhvankoAnna@users.noreply.github.com>
2023-03-24 20:39:57 +0000

    Deploying to gh-pages from @ ZhvankoAnna/wallet@86ad6a9464c1e9150a9a5f492f104da682cb59f2 🚀

---
+++ static/js/312.abfd9559.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[312],{6312:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var a="not-found-page_title__O3HUi",r=n(3329),c=function(){return(0,r.jsx)("div",{className:"container",children:(0,r.jsx)("h2",{className:a,children:"Not Found page"})})}}}]);
+//# sourceMappingURL=312.abfd9559.chunk.js.map
---
+++ static/js/872.6114fd18.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[872],{3872:function(e,t,r){r.r(t);var a=r(2912),n=r(3329);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)(a.Z,{})})}}}]);
+//# sourceMappingURL=872.6114fd18.chunk.js.map
